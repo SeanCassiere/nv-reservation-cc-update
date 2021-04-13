22c35d2250220ac8f1a918a0a7386ecba6b3dcd4
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-04-13 12:54:50 +0530

    NoQueryString page styled

--- src/layouts/QueryStringNotPasses.jsx
+++ src/layouts/QueryStringNotPasses.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+
+import NotFoundImg from "../assets/undraw_page_not_found_su7k.svg";
 
 const QueryStringNotPasses = () => {
-	return <div>Query string not passed</div>;
+	return (
+		<Card style={{ width: "25em" }}>
+			<Card.Img variant='top' alt='Not Found' src={NotFoundImg} />
+			<Card.Body>
+				<Card.Title>Uh-Oh</Card.Title>
+				<Card.Text>
+					<p>It looks like all the necessary information was not sent.</p>
+					<p>
+						Please report this issue to&nbsp;
+						<a href='mailto:support@navotar.com' target='_blank' rel='noreferrer'>
+							support@navotar.com
+						</a>
+					</p>
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default QueryStringNotPasses;
