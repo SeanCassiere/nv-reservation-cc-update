e748a5569bf589cbe3346424bb2262b105b7244e
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-04-13 12:31:23 +0530

    Added homepage styling and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import QueryStringNotPassed from "./layouts/QueryStringNotPasses";
 import UserApplicationForm from "./layouts/UserApplicationForm";
@@ -10,14 +10,26 @@ const queryClientId = new URLSearchParams(window.location.search).get("clientId"
 const App = () => {
 	return (
 		<Container>
-			{queryClientId !== null &&
-			queryReservationId !== null &&
-			queryClientId.length !== 0 &&
-			queryReservationId.length !== 0 ? (
-				<UserApplicationForm clientId={queryClientId} reservationId={queryReservationId} />
-			) : (
-				<QueryStringNotPassed />
-			)}
+			<Row className='justify-content-lg-center' style={{ paddingTop: "1rem" }}>
+				<Col xs={12} sm={12} md={4} lg={4}>
+					{queryClientId !== null &&
+					queryReservationId !== null &&
+					queryClientId.length !== 0 &&
+					queryReservationId.length !== 0 ? (
+						<UserApplicationForm clientId={queryClientId} reservationId={queryReservationId} />
+					) : (
+						<QueryStringNotPassed />
+					)}
+				</Col>
+				<Col xs={12} sm={12} md={12} lg={12}>
+					<p style={{ padding: "1rem 0" }} className='text-center'>
+						Powered by{" "}
+						<a href='https://navotar.com' target='_blank' rel='noreferrer' className='text-primary'>
+							Navotar
+						</a>
+					</p>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
