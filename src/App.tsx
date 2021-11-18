fd4002862f0dddf79afc89204ab307a0c63c3f27
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-11-18 17:16:15 +0530

    feat: add React Error Boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
 
 import AppRoutes from "./routes/AppRoutes";
 import { selectTranslations } from "./shared/redux/store";
+import ErrorSubmission from "./shared/pages/ErrorSubmission/ErrorSubmission";
 
 const App = () => {
 	const t = useSelector(selectTranslations);
@@ -12,7 +14,9 @@ const App = () => {
 		<Container>
 			<Row className='justify-content-lg-center' style={{ paddingTop: "1rem" }}>
 				<Col xs={12} sm={12} md={12} lg={6}>
-					<AppRoutes />
+					<ErrorBoundary FallbackComponent={ErrorFallback}>
+						<AppRoutes />
+					</ErrorBoundary>
 				</Col>
 				<Col xs={12} sm={12} md={12} lg={12}>
 					<p style={{ padding: "1rem 0" }} className='text-center'>
@@ -25,6 +29,10 @@ const App = () => {
 			</Row>
 		</Container>
 	);
+};
+
+const ErrorFallback = () => {
+	return <ErrorSubmission msg={"The application encountered a fatal error."} />;
 };
 
 export default App;
