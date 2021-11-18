8fe7ebf1344a7b4eb9c498426ffedfc1ae78cdc2
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-11-18 16:11:45 +0530

    feat: added license image selection ui

---
+++ src/shared/components/DefaultImageDropzoneWithPreview/DefaultImageDropzoneWithPreview.tsx
@@ -0,0 +1,113 @@
+import React, { useCallback, useMemo, useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+import Figure from "react-bootstrap/esm/Figure";
+import { useDropzone } from "react-dropzone";
+
+const baseStyle = {
+	flex: 1,
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	justifyContent: "center",
+	padding: "20px 20px",
+	borderWidth: 2,
+	borderRadius: 2,
+	borderColor: "#eeeeee",
+	borderStyle: "dashed",
+	backgroundColor: "#fafafa",
+	color: "#bdbdbd",
+	outline: "none",
+	transition: "border .24s ease-in-out",
+};
+
+const activeStyle = {
+	borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+	borderColor: "#00e676",
+};
+
+const rejectStyle = {
+	borderColor: "#ff1744",
+};
+
+interface IProps {
+	dragDisplayText: string;
+	selectButtonText: string;
+	clearButtonText: string;
+	onSelectFile?: (file: File) => void;
+}
+
+const DefaultImageDropzoneWithPreview = ({
+	dragDisplayText,
+	clearButtonText,
+	selectButtonText,
+	onSelectFile,
+}: IProps) => {
+	const [previewImage, setPreviewImage] = useState<{ fileName: string; url: string } | null>(null);
+
+	const onDrop = useCallback(
+		(acceptedFiles: File[]) => {
+			const file = acceptedFiles[0];
+			const objectUrl = URL.createObjectURL(file);
+			setPreviewImage({ fileName: file.name, url: objectUrl });
+
+			if (onSelectFile) {
+				// callBack to lift file to parent state
+				onSelectFile(file);
+			}
+		},
+		[onSelectFile]
+	);
+
+	const handleClearImage = useCallback(() => {
+		setPreviewImage(null);
+	}, []);
+
+	const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject, open } = useDropzone({
+		accept: "image/*",
+		noClick: true,
+		noKeyboard: true,
+		maxFiles: 1,
+		onDrop,
+	});
+
+	const style = useMemo(
+		() => ({
+			...baseStyle,
+			...(isDragActive ? activeStyle : {}),
+			...(isDragAccept ? acceptStyle : {}),
+			...(isDragReject ? rejectStyle : {}),
+		}),
+		[isDragActive, isDragAccept, isDragReject]
+	);
+	return (
+		<>
+			<div {...getRootProps({ style: style as any })}>
+				<input {...getInputProps()} />
+				{previewImage ? (
+					<Figure style={{ width: "100%", textAlign: "center", margin: 0 }}>
+						<Figure.Image alt={previewImage.fileName} src={previewImage.url} style={{ height: "130px" }} />
+						<Figure.Caption>{previewImage.fileName}</Figure.Caption>
+					</Figure>
+				) : (
+					<p>{dragDisplayText}</p>
+				)}
+			</div>
+			<div className='mt-2'>
+				{previewImage ? (
+					<Button variant='danger' style={{ width: "100%" }} onClick={handleClearImage}>
+						{clearButtonText}
+					</Button>
+				) : (
+					<Button variant='secondary' style={{ width: "100%" }} onClick={open}>
+						{selectButtonText}
+					</Button>
+				)}
+			</div>
+		</>
+	);
+};
+
+export default DefaultImageDropzoneWithPreview;
--- src/shared/controllers/LicenseUploadController/DefaultLicenseUploadController.tsx
+++ src/shared/controllers/LicenseUploadController/DefaultLicenseUploadController.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useState } from "react";
-import { Card, Row, Col, Button, Modal } from "react-bootstrap";
+import { Card, Row, Col, Button, Modal, Accordion, Alert } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 import { selectTranslations } from "../../redux/store";
+import DefaultImageDropzoneWithPreview from "../../components/DefaultImageDropzoneWithPreview/DefaultImageDropzoneWithPreview";
 
 interface IProps {
 	handleSubmit: () => void;
@@ -18,11 +19,42 @@ const DefaultLicenseUploadController = ({
 	isPrevPageAvailable,
 }: IProps) => {
 	const t = useSelector(selectTranslations);
-	const [returnModalOpen, setReturnModalOpen] = useState(false);
+	const [key, setKey] = useState("front");
 
+	const [frontImage, setFrontImage] = useState<File | null>(null);
+	const [displayNoFrontImageError, setDisplayNoFrontImageError] = useState(false);
+	const [backImage, setBackImage] = useState<File | null>(null);
+	const [displayNoBackImageError, setDisplayNoBackImageError] = useState(false);
+
+	const selectFrontImage = useCallback((file: File) => {
+		setDisplayNoFrontImageError(false);
+		setFrontImage(file);
+		setTimeout(() => {
+			setKey("back");
+		}, 500);
+	}, []);
+
+	const selectBackImage = useCallback((file: File) => {
+		setDisplayNoBackImageError(false);
+		setBackImage(file);
+	}, []);
+
+	// General component state
+	const [returnModalOpen, setReturnModalOpen] = useState(false);
 	const handleNextState = useCallback(() => {
+		if (!frontImage) {
+			setKey("front");
+			setDisplayNoFrontImageError(true);
+			return;
+		}
+		if (!backImage) {
+			setKey("back");
+			setDisplayNoBackImageError(true);
+			return;
+		}
+
 		handleSubmit();
-	}, [handleSubmit]);
+	}, [backImage, frontImage, handleSubmit]);
 
 	const handleOpenModalConfirmation = useCallback(() => {
 		setReturnModalOpen(true);
@@ -39,7 +71,7 @@ const DefaultLicenseUploadController = ({
 		<>
 			<Modal show={returnModalOpen} onHide={handleModalDenyReturn} keyboard={true}>
 				<Modal.Header>Are you sure?</Modal.Header>
-				<Modal.Body>Going back to the previous page will discard all added files.</Modal.Body>
+				<Modal.Body>Going back will discard the added files.</Modal.Body>
 				<Modal.Footer>
 					<Button variant='secondary' onClick={handleModalDenyReturn}>
 						Cancel
@@ -52,9 +84,48 @@ const DefaultLicenseUploadController = ({
 			<Card border='light'>
 				<Card.Body>
 					<Card.Title>Driver's License</Card.Title>
-					<Card.Subtitle>Upload a copy of your driver's license</Card.Subtitle>
-					<div className='mt-4 d-grid'>
+					<Card.Subtitle>Upload front and back images of your driver's license.</Card.Subtitle>
+					<div className='mt-3 d-grid'>
 						<Row>
+							<Col md={12}>
+								<Accordion
+									// defaultActiveKey='front'
+									activeKey={key}
+									onSelect={(k) => {
+										if (k) setKey(k);
+									}}
+									className='border-light'
+								>
+									<Accordion.Item eventKey='front' className='border-light' style={{ background: "none" }}>
+										<Accordion.Header className='border-light' style={{ padding: "0px !important" }}>
+											Front Image
+										</Accordion.Header>
+										<Accordion.Body>
+											{displayNoFrontImageError && <Alert variant='light'>You must select the front image.</Alert>}
+											<DefaultImageDropzoneWithPreview
+												dragDisplayText='Drag and drop the front image here'
+												selectButtonText='Select the front image'
+												clearButtonText='Clear the front image'
+												onSelectFile={selectFrontImage}
+											/>
+										</Accordion.Body>
+									</Accordion.Item>
+									<Accordion.Item eventKey='back' className='border-light'>
+										<Accordion.Header className='border-light'>Back Image</Accordion.Header>
+										<Accordion.Body>
+											{displayNoBackImageError && <Alert variant='light'>You must select the back image.</Alert>}
+											<DefaultImageDropzoneWithPreview
+												dragDisplayText='Drag and drop the back image here'
+												selectButtonText='Select the back image'
+												clearButtonText='Clear the back image'
+												onSelectFile={selectBackImage}
+											/>
+										</Accordion.Body>
+									</Accordion.Item>
+								</Accordion>
+							</Col>
+						</Row>
+						<Row className='mt-3'>
 							{isPrevPageAvailable() && (
 								<Col xs={2} className='pr-0'>
 									<Button variant='warning' size='lg' style={{ width: "100%" }} onClick={handleOpenModalConfirmation}>
@@ -74,5 +145,4 @@ const DefaultLicenseUploadController = ({
 		</>
 	);
 };
-
 export default DefaultLicenseUploadController;
