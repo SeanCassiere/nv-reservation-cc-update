bc0464d002f3cd36a34b590c3970cc3487e87324
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-01-03 19:49:52 +0530

    fetch access_token

---
+++ netlify/functions/getV3Token/getV3Token.ts
@@ -0,0 +1,23 @@
+import { Handler } from "@netlify/functions";
+import axios from "axios";
+
+export const handler: Handler = async (event, context) => {
+	try {
+		const params = new URLSearchParams();
+		params.append("grant_type", "client_credentials");
+		params.append("client_id", process.env.REACT_APP_V3_CLIENT_ID);
+		params.append("client_secret", process.env.REACT_APP_V3_CLIENT_SECRET);
+		params.append("scope", "Api");
+		const { data } = await axios.post(process.env.V3_AUTH_URL, params, {
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+		});
+
+		return { statusCode: 200, body: JSON.stringify(data) };
+	} catch (error) {
+		console.log(error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ error: "Failed fetching data" }),
+		};
+	}
+};
