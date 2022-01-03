251b7a382043b43a2f121283adb0440d3c750c2b
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-01-03 20:12:06 +0530

    don't stringify response

--- netlify/functions/getV3Token/getV3Token.ts
+++ netlify/functions/getV3Token/getV3Token.ts
@@ -12,7 +12,7 @@ export const handler: Handler = async (event, context) => {
 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
 		});
 
-		return { statusCode: 200, body: JSON.stringify(data) };
+		return { statusCode: 200, body: data };
 	} catch (error) {
 		console.log(error);
 		return {
