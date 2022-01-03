53d38a82019e4be15636680dbf8857c4ac9f8a74
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-01-03 20:01:24 +0530

    edited secrets

--- netlify/functions/getV3Token/getV3Token.ts
+++ netlify/functions/getV3Token/getV3Token.ts
@@ -5,8 +5,8 @@ export const handler: Handler = async (event, context) => {
 	try {
 		const params = new URLSearchParams();
 		params.append("grant_type", "client_credentials");
-		params.append("client_id", process.env.REACT_APP_V3_CLIENT_ID);
-		params.append("client_secret", process.env.REACT_APP_V3_CLIENT_SECRET);
+		params.append("client_id", process.env.V3_CLIENT_ID);
+		params.append("client_secret", process.env.V3_CLIENT_SECRET);
 		params.append("scope", "Api");
 		const { data } = await axios.post(process.env.V3_AUTH_URL, params, {
 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
