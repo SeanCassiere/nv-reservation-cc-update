abec2d5c57b16039ebd9e06d5bae2c4546343919
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-07-27 23:19:40 +0530

    chore: change order

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import HttpApi from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import { supportedLanguages } from "./shared/redux/slices/config/slice";
 
