94d3e807433ed47b8617ba0db52e5363cf626758
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-08-25 15:13:12 +0530

    fix: content-type of application-json when getting an access token
    content-type of application-json when getting an access token

--- netlify/functions/token.ts
+++ netlify/functions/token.ts
@@ -2,45 +2,46 @@ import { builder, Handler } from "@netlify/functions";
 import axios from "axios";
 
 const containsKey = (rawQueryString: string, key: string): boolean => {
-	return rawQueryString.indexOf(key) !== -1;
+  return rawQueryString.indexOf(key) !== -1;
 };
 
 const isValueTrue = (value: string | undefined | null | boolean): boolean => {
-	if (!value) return false;
-	return value &&
-		((typeof value === "string" && value.toLowerCase() === "true") ||
-			(typeof value === "string" && value === "1") ||
-			value === true)
-		? true
-		: false;
+  if (!value) return false;
+  return value &&
+    ((typeof value === "string" && value.toLowerCase() === "true") ||
+      (typeof value === "string" && value === "1") ||
+      value === true)
+    ? true
+    : false;
+};
+
+const responseHeaders = {
+  "Content-Type": "application/json",
 };
 
 const myHandler: Handler = async (event, context) => {
-	const qaQuery = containsKey(event.rawQuery, "qa");
-	const isQa = Boolean(isValueTrue(qaQuery));
+  const qaQuery = containsKey(event.rawQuery, "qa");
+  const isQa = Boolean(isValueTrue(qaQuery));
 
-	const url = isQa ? process.env.QA_V3_AUTH_URL! : process.env.V3_AUTH_URL!;
-	const clientId = isQa ? process.env.QA_V3_CLIENT_ID! : process.env.V3_CLIENT_ID!;
-	const clientSecret = isQa ? process.env.QA_V3_CLIENT_SECRET! : process.env.V3_CLIENT_SECRET!;
+  const url = isQa ? process.env.QA_V3_AUTH_URL! : process.env.V3_AUTH_URL!;
+  const clientId = isQa ? process.env.QA_V3_CLIENT_ID! : process.env.V3_CLIENT_ID!;
+  const clientSecret = isQa ? process.env.QA_V3_CLIENT_SECRET! : process.env.V3_CLIENT_SECRET!;
 
-	try {
-		const params = new URLSearchParams();
-		params.append("grant_type", "client_credentials");
-		params.append("client_id", clientId);
-		params.append("client_secret", clientSecret);
-		params.append("scope", "Api");
-		const { data } = await axios.post(url, params, {
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-		});
+  try {
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+    params.append("client_id", clientId);
+    params.append("client_secret", clientSecret);
+    params.append("scope", "Api");
+    const { data } = await axios.post(url, params, {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
 
-		return { statusCode: 200, body: JSON.stringify(data) };
-	} catch (error) {
-		console.log(error);
-		return {
-			statusCode: 500,
-			body: JSON.stringify({ error: error }),
-		};
-	}
+    return { statusCode: 200, body: JSON.stringify(data), headers: responseHeaders };
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 500, body: JSON.stringify({ error: error }), headers: responseHeaders };
+  }
 };
 
 const handler = builder(myHandler);
