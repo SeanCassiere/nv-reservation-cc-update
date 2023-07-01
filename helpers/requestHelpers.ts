5112f4a73274a8ab369f35cf9db99295857154c7
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-01 23:33:45 +1200

    refactor: making the authentication lambdas a POST request instead of GET (#222)
    * refactor: rewrite authentication lambdas to be POST only
    
    * refactor: use the new POST authentication lambdas

--- helpers/requestHelpers.ts
+++ helpers/requestHelpers.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 export const containsKey = (rawQueryString: string, key: string): boolean => {
   return rawQueryString.indexOf(key) !== -1;
 };
@@ -16,3 +17,9 @@ export const responseHeaders = {
   "Content-Type": "application/json",
   "Cache-Control": "no-store",
 };
+
+export const tokenRequestSchema = z.object({
+  client_id: z.string().min(1, "Client ID must be at least 1 character"),
+  reference_type: z.string().min(1, "Reference type must be at least 1 character"),
+  reference_id: z.string().min(1, "Reference ID must be at least 1 character"),
+});
