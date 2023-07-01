2cc7f8e510ddb6328ba271e6a147b23df7d763b7
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-01 20:11:52 +1200

    feat: custom i18next declartion file

--- src/i18next.d.ts
+++ src/i18next.d.ts
@@ -1,16 +1,12 @@
-import "react-i18next";
+import "i18next";
 import translationEn from "../public/locales/en/translation.json";
 import translationCommon from "../public/locales/common/translation.json";
 
 const defaultAndDev = { ...translationEn, ...translationCommon };
 
-// react-i18next versions higher than 11.11.0
-declare module "react-i18next" {
-  // and extend them!
+declare module "i18next" {
   interface CustomTypeOptions {
-    // custom resources type
-    resources: {
-      defaultNS: typeof defaultAndDev;
-    };
+    defaultNS: "translation";
+    resources: typeof defaultAndDev;
   }
 }
