3e9d57d15bd46d582424a753e066d5cd2155e55f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-06 11:23:15 +1200

    chore: add comments to describe the AppNavMode type

--- src/hooks/logic/useAppNavContext.tsx
+++ src/hooks/logic/useAppNavContext.tsx
@@ -12,7 +12,17 @@ const pagesTyped: { page: string; type: PageTypeOptions }[] = [
   { page: APP_CONSTANTS.FLOW_RENTAL_SIGNATURE_FORM, type: "rentalSignature" },
 ];
 
+/**
+ * "navigate" - Refers to the default navigation flow.
+ *
+ * In this state, the user can navigate between the screens using the next and previous buttons.
+ *
+ * "save" - Refers to the state where the user is editing a page.
+ *
+ * In this state, the pressing of the next button will take the user to the last page of the flow.
+ */
 export type AppNavMode = "navigate" | "save";
+
 type AppNavContextType = {
   mode: AppNavMode;
   goNext: () => void;
