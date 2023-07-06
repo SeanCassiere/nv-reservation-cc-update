cffb351bf8e9737103f868484014c27713029ea5
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-06 17:14:40 +1200

    feat: validate api response for fetching admin user

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,36 +1,20 @@
+import { z } from "zod";
+
 import { clientFetch } from "./clientV3";
 
-type SystemUser = {
-  clientId: number;
-  userID: number;
-  userName: string;
-  firstName: string | null;
-  lastName: string | null;
-  fullName: null;
-  email: string | null;
-  userRoleID: number;
-  language: null;
-  roleName: "Admin";
-  locationName: string | null;
-  lastLogin: string | null;
-  isReservationEmail: boolean;
-  isActive: true;
-  phone: string | null;
-  scanAccessKey: string | null;
-  languageName: string | null;
-  lockOut: boolean;
-  locationList: null;
-  userIdV3: string | null;
-  overrideDateFormat: string | null;
-  overrideTimeFormat: string | null;
-};
+const systemUserSchema = z.object({
+  userRoleID: z.number().nullable(),
+  userID: z.number(),
+});
 
 export async function fetchAdminUser(clientId: string | number) {
   const params = new URLSearchParams();
   params.append("clientId", `${clientId}`);
 
   try {
-    const users = (await clientFetch("/Users?" + params).then((r) => r.json())) as SystemUser[];
+    const users = await clientFetch("/Users?" + params)
+      .then((r) => r.json())
+      .then((data) => z.array(systemUserSchema).parse(data ?? []));
     const adminUserIdToUse = users.filter((u) => u.userRoleID === 1);
     if (adminUserIdToUse.length === 0) {
       throw new Error("No admin user found");
