8c98807fd888c7d5ef66b8babcbd65bbb5eb690d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-07 15:12:16 +1200

    feat: check navigation context to customize the confirmation message for license upload

--- src/controllers/cc-and-license-upload-default.tsx
+++ src/controllers/cc-and-license-upload-default.tsx
@@ -116,7 +116,7 @@ const DefaultCreditCardAndLicenseUploadController: React.FC<IProps> = () => {
         <GoBackConfirmationDialog
           isOpen={isBackConfirmationDialogOpen}
           title={t("forms.licenseUpload.goBack.title")}
-          message={t("forms.licenseUpload.goBack.message")}
+          message={t("forms.licenseUpload.goBack.message", { context: mode === "save" ? "edit" : "" })}
           onCancel={dismissBackDialog}
           onConfirm={confirmBackDialog}
           cancelBtnText={t("forms.licenseUpload.goBack.cancel")}
