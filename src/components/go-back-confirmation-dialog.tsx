32e19fcd54bee04d8de0c8e447ebf09323a143a8
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-07-07 14:57:39 +1200

    feat: migrate the go-back-confirmation-dialog to radix with shadcn

--- src/components/go-back-confirmation-dialog.tsx
+++ src/components/go-back-confirmation-dialog.tsx
@@ -1,8 +1,15 @@
-import React, { Fragment } from "react";
-import { Transition, Dialog } from "@headlessui/react";
+import React from "react";
 
-import { ExclamationTriangleIcon } from "@/components/ui/icons";
-import { Button as UIButton } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 interface Props {
   isOpen: boolean;
@@ -16,59 +23,17 @@ interface Props {
 
 export const GoBackConfirmationDialog: React.FC<Props> = React.memo((props) => {
   return (
-    <Transition.Root show={props.isOpen} as={Fragment}>
-      <Dialog as="div" onClose={props.onCancel} className="fixed inset-0 z-50 overflow-y-auto">
-        <div className="flex min-h-screen items-center justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-background/75 transition-opacity" />
-          </Transition.Child>
-
-          <span className="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true">
-            &#8203;
-          </span>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            <div className="relative inline-block transform overflow-hidden rounded border border-accent bg-background px-4 pb-4 pt-5 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
-              <div>
-                <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-warning-foreground text-warning">
-                  <ExclamationTriangleIcon />
-                </div>
-                <div className="mt-3 text-center sm:mt-5">
-                  <Dialog.Title as="h3" className="text-2xl font-medium leading-6 text-primary">
-                    {props.title}
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-primary/80">{props.message}</p>
-                  </div>
-                  <div className="mt-10 flex flex-col gap-2">
-                    <UIButton type="button" onClick={props.onConfirm} size="sm" autoFocus>
-                      {props.confirmBtnText}
-                    </UIButton>
-                    <UIButton type="button" onClick={props.onCancel} size="sm" variant="ghost">
-                      {props.cancelBtnText}
-                    </UIButton>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root>
+    <AlertDialog open={props.isOpen} onOpenChange={props.onCancel}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{props.title}</AlertDialogTitle>
+          <AlertDialogDescription>{props.message}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>{props.cancelBtnText}</AlertDialogCancel>
+          <AlertDialogAction onClick={props.onConfirm}>{props.confirmBtnText}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 });
