95e41f5cbe6ad42926e6b936fde4384cb9f258fe
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-08-22 17:11:48 +1200

    style: configure `@ianvs/prettier-plugin-sort-imports`

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -6,7 +6,22 @@ module.exports = {
   printWidth: 120,
   jsxSingleQuote: false,
   plugins: [
+    require.resolve("@ianvs/prettier-plugin-sort-imports"),
     require.resolve("prettier-plugin-tailwindcss"), // MUST come last
   ],
   tailwindConfig: "./tailwind.config.js",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/components/(.*)$",
+    "",
+    "^@/(.*)$",
+    "",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
 };
