0ea68206eea558dc393e610ea6ac2b14e3a82d21
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2023-10-19 13:27:57 +1300

    refactor: remove unneeded

--- src/hooks/network/useRentalSummary.ts
+++ src/hooks/network/useRentalSummary.ts
@@ -1,27 +1,23 @@
-import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
-import { fetchRentalSummary, type SummaryCharges } from "@/api/summaryApi";
+import { fetchRentalSummary } from "@/api/summaryApi";
 
-export const useRentalSummaryQuery = (
-  {
-    clientId,
-    referenceId,
-    referenceType,
-  }: {
-    clientId: string | number;
-    referenceId: string | number;
-    referenceType: string;
-  },
-  queryOpts?: UseQueryOptions<SummaryCharges>,
-) => {
+export const useRentalSummaryQuery = ({
+  clientId,
+  referenceId,
+  referenceType,
+}: {
+  clientId: string | number;
+  referenceId: string | number;
+  referenceType: string;
+}) => {
   return useQuery({
     queryKey: ["rental", "summary-of-charges"],
-    queryFn: async () =>
+    queryFn: () =>
       fetchRentalSummary({
         clientId: clientId,
         referenceId: referenceId,
         referenceType: referenceType.trim().toLowerCase() === "agreement" ? "Agreement" : "Reservation",
       }),
-    ...queryOpts,
   });
 };
